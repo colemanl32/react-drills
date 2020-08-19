b8c5b50cfdc5f8144f77534d6d4046cffa1ba360
Coleman Lindquist <coleman.lindquist.32@gmail.com>
2020-08-19 15:36:24 -0600

    React Drills

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    
+    this.state = {
+      games: ["Skyrim", "Crusader Kings", "Sekiro", "Mount and Blade", "Minecraft"]
+    }
+  }
+  render(){
+    let gamesDisplay = this.state.games.map((element, index) => {
+      return <h2 key = {index}> {element} </h2>
+    })
+  return <div className = "App"> {gamesDisplay} </div>
+  }
 }
 
 export default App;
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filterString: "",
+      games: ["Sekiro", "Skyrim", "Crusader Kings", "Minecraft", "Diablo"]
+    };
+  }
+
+  handleChange(filter) {
+    this.setState({ filterString: filter });
+  }
+
+  render() {
+    let gamesDisplay = this.state.games
+      .filter((element, index) => {
+        return element.includes(this.state.filterString);
+      })
+      .map((element, index) => {
+        return <h2 key={index}>{element}</h2>;
+      });
+
+    return (
+      <div className="App">
+        <input onChange={e => this.handleChange(e.target.value)} type="text" />
+        {gamesDisplay}
+      </div>
+    );
+  }
 }
 
 export default App;
